8c1eb1c79dddfe19f3cf99c3abf994ef2c33f22b
justin <ohiojustin0@gmail.com>
2026-07-20 15:12:16 +0800

    testing

--- proyek1.js
+++ proyek1.js
@@ -1,16 +1,23 @@
-const nama = "budi";
+const nama = "Budi";
 const umur = 20;
 const hargaBarang = [12000, 15000, 8000, 5000];
 
-let totalHarga = 0;
+let total = 0;
 
 for (let i = 0; i < hargaBarang.length; i++) {
-    totalHarga += hargaBarang[i];
+    total += hargaBarang[i];
 }
 
-let status = mendapatkanDiskon(totalHarga);
+let status;
 
-{
-    if (total > 30000) {
-        status = "mendapatkan diskon";
-    console log
+if (total > 30000) {
+    status = "Mendapat Diskon";
+    console.log("Selamat! Anda mendapatkan diskon.");
+} else {
+    status = "Belanja lagi agar mendapat diskon.";
+}
+
+console.log("Nama :", nama);
+console.log("Umur :", umur);
+console.log("Total Belanja :", total);
+console.log("Status :", status);
