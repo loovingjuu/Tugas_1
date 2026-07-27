02aa18e18667ac07c3d36f16e311eef500fb7c1c
justin <ohiojustin0@gmail.com>
2026-07-27 13:40:02 +0800

    tes

--- proyek10.js
+++ proyek10.js
@@ -1,16 +1,17 @@
 const skor = [100, 450, 800, 150, 900];
 
 for (let i = 0; i < skor.length; i++) {
-    if (skor[i] >= 800) {
-        console.log(`player ${i + 1} -> ${skor[i]} -> mythic`);
-    }
-    else if (skor[i] >= 500) {
-        console.log(`player ${i + 1} -> ${skor[i]} -> legend`);
-    }
-    else if (skor[i] >= 300) {
-        console.log(`player ${i + 1} -> ${skor[i]} -> epic`);
-    }
-    else {
-        console.log(`player ${i + 1} -> ${skor[i]} -> warrior`);
-    }
+
+let rank = '';
+if (skor[i] >= 800) {
+    rank = 'mythic';
+} else if (skor[i] >= 500) {
+    rank = 'legend';
+} else if (skor[i] >= 300) {
+    rank = 'epic';
+} else {
+    rank = 'warrior';
+}
+
+    console.log(`Player ke-${i + 1} : ${skor[i]} -> Rank: ${rank}`);
 }
